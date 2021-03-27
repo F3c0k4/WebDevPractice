62b25011b17a87d6db2eb5a730954e5325f3ab4e
F3c0k4 <32798351+F3c0k4@users.noreply.github.com>
2021-03-27 14:36:21 +0100

    Practicing passing data into templates

--- Templates/main.go
+++ Templates/main.go
@@ -6,12 +6,33 @@ import (
 	"text/template"
 )
 
-func main() {
+var mytemp *template.Template
 
-	mytemp, err := template.ParseGlob("templates/*.gohtml")
+type dummy struct {
+	A int
+	B string
+}
+
+func init() {
+	mytemp = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	d := dummy{A: 4, B: "hello there"}
+	e := []dummy{{A: 443, B: "I'm blue"}, {A: 321, B: "Da ba dee da ba die"}}
+	mytemp.ExecuteTemplate(os.Stdout, "nodata.gohtml", nil)
+
+	mytemp.ExecuteTemplate(os.Stdout, "datapassedsimply.gohtml", 22)
+
+	mytemp.ExecuteTemplate(os.Stdout, "datathroughvar.gohtml", "this is my string")
+	mytemp.ExecuteTemplate(os.Stdout, "dataloopedthrough.gohtml", []string{"the first", "the second", "the thrid", "and the fourth"})
+	mytemp.ExecuteTemplate(os.Stdout, "dataloopedthroughvar.gohtml", []string{"the first", "the second", "the thrid", "and the fourth"})
+	err := mytemp.ExecuteTemplate(os.Stdout, "datapassedthroughstruct.gohtml", d)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	mytemp.ExecuteTemplate(os.Stdout, "first.gohtml", nil)
+	err = mytemp.ExecuteTemplate(os.Stdout, "datapassedthroughstructlooped.gohtml", e)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
