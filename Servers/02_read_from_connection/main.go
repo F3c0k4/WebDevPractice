68fa82d57895cee6e928b521c82032f7805cecf1
F3c0k4 <32798351+F3c0k4@users.noreply.github.com>
2021-03-30 19:24:19 +0200

    read from connection

---
+++ Servers/02_read_from_connection/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic()
+	}
+	defer li.Close()
+
+	for {
+		con, err := li.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		go handle(con)
+	}
+
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
+	fmt.Println("Code got here")
+}
