709b6f36ef49bcf232b5bce7d62b4058cccb74f6
F3c0k4 <32798351+F3c0k4@users.noreply.github.com>
2021-04-07 20:31:45 +0200

    restructuring and servecontent tryout

---
+++ Servers/06_servefile/02_with_http/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/honda", serveHonda)
+	http.ListenAndServe(":8080", nil)
+}
+
+func serveHonda(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("honda.jpg")
+	if err != nil {
+		http.Error(w, "File not found", 404)
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		http.Error(w, "File not found", 404)
+	}
+
+	http.ServeContent(w, req, fi.Name(), fi.ModTime(), f)
+}
